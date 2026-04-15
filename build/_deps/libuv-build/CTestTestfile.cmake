1f746fa6cc1a52bbadda0d0ee2967c4ee45bfedd
Mxqwthl <1377166156@qq.com>
2026-04-15 18:19:28 +0800

    1

--- build/_deps/libuv-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mxqwthl/bitbot_cifx_demo/build/_deps/libuv-src
-# Build directory: /home/mxqwthl/bitbot_cifx_demo/build/_deps/libuv-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/_deps/pugixml-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mxqwthl/bitbot_cifx_demo/build/_deps/pugixml-src
-# Build directory: /home/mxqwthl/bitbot_cifx_demo/build/_deps/pugixml-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
